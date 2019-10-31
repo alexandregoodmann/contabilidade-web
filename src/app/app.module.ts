0ddad4a7c04dc730882f81cbc2735c77bd359a7c
unknown <alexandregoodmann@gmail.com>
2019-10-31 10:14:25 -0300

    fix routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -74,7 +74,7 @@ const routes: Routes = [
     ProgressBarComponent
   ],
   imports: [
-    RouterModule.forRoot(routes, { useHash: false }),
+    RouterModule.forRoot(routes, { useHash: true }),
     BrowserModule,
     BrowserAnimationsModule,
     //MATERIAL
