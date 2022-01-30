cd540b4e305cc7f140b3a971cd04ac63102ca0d2
iam_afesilva <iam_afesilva@idcontractors.net>
2022-01-30 16:48:23 -0300

    Mudança menu principal

--- src/app/principal/principal.component.ts
+++ src/app/principal/principal.component.ts
@@ -8,12 +8,12 @@ import { Component } from '@angular/core';
 export class PrincipalComponent {
 
   banners = [
-    { href: '/#/lancamento', icon: 'monetization_on', label: 'Lançamento' },
-    { href: '/#/extrato', icon: 'receipt', label: 'Extrato' },
     { href: '/#/banco', icon: 'account_balance', label: 'Banco' },
-    { href: '/#/categoria', icon: 'dashboard_customize', label: 'Categoria' },
     { href: '/#/conta', icon: 'credit_card', label: 'Conta' },
-    { href: '/#/carga', icon: 'input', label: 'Carga de Lançamentos' }
+    { href: '/#/categoria', icon: 'dashboard_customize', label: 'Categoria' },
+    { href: '/#/lancamento', icon: 'monetization_on', label: 'Lançamento' },
+    { href: '/#/extrato', icon: 'receipt', label: 'Extrato' }
+    // { href: '/#/carga', icon: 'input', label: 'Carga de Lançamentos' }
   ];
 
   constructor() { }
